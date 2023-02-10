f8a668e33acdaa6f1a701e01d0d2affb144bec7b
albertjacobsz <dekater403@gmail.com>
2023-02-11 02:46:07 +0800

    Finished Task 0 and Task 1.1 & T1.2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,35 @@
-import Head from 'next/head'
-import GoogleButton from 'react-google-button'
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
-import {useRouter} from "next/router";
+import Head from 'next/head';
+import GoogleButton from 'react-google-button';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithRedirect,
+  getRedirectResult,
+} from 'firebase/auth';
+import { useRouter } from 'next/router';
 import { initializeApp } from 'firebase/app';
 
 // Task 0: Initialize Firebase
 // Replace the following with your app's Firebase project configuration
 // https://firebase.google.com/docs/web/setup
+
+//Unsecure way of storing the API key
 const firebaseConfig = {
-  // Enter your own firebase config here
+  apiKey: 'AIzaSyBO4LEieQNAuG2w_s2ActgtKSyqp6GaRGE',
+  authDomain: 'checkmate-interview.firebaseapp.com',
+  projectId: 'checkmate-interview',
+  storageBucket: 'checkmate-interview.appspot.com',
+  messagingSenderId: '596221673049',
+  appId: '1:596221673049:web:b589dd84fb353884fb7b48',
+  measurementId: 'G-F6DTM4PFEG',
 };
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // GoogleAuthProvider instance
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 // Firebase Auth instance
-// const auth = getAuth(app);
+const auth = getAuth(app);
 
 export default function Home() {
   //Next.js router
@@ -24,16 +37,19 @@ export default function Home() {
 
   // Task 1: Implement Google Sign in with Firebase
   // https://firebase.google.com/docs/auth/web/google-signin
+  console.log('hello');
   const signIn = () => {
+    signInWithRedirect(auth, provider);
+
+    router.push('/signed-in', '/signed-in');
+
     /*
       1. Use the GoogleAuthProvider to sign in with Firebase
       2. Use signInWithRedirect to redirect the user to the Google sign in page
       3. (Optional) Use getRedirectResult to get the result of the redirect and check out what is inside :)
       4. Redirect the user to the signed-in page using Next.js router
      */
-
-  }
-
+  };
   return (
     <>
       <Head>
@@ -41,7 +57,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="container">
-        <main style={{display: 'flex', justifyContent:'center', alignItems:'center', flexDirection: 'column'}}>
+        <main
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+        >
           <h1 className="title">
             Welcome to <a href="https://checkmatehk.io">CheckMate</a>
           </h1>
@@ -52,11 +75,18 @@ export default function Home() {
           <GoogleButton
             label={'Sign in with Google'}
             type="light"
-            style={{ width: '50%', display:"flex", justifyContent: 'center', alignItems: 'center', fontFamily: 'Roboto, sans-serif', color:'#444' }}
+            style={{
+              width: '50%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontFamily: 'Roboto, sans-serif',
+              color: '#444',
+            }}
             onClick={signIn}
           />
         </main>
       </div>
-      </>
-  )
+    </>
+  );
 }
